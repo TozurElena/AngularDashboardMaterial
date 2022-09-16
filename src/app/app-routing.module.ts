878a01fc276a171550bf9700234e129e31de17d9
Tom Depasse <la196807@student.helha.be>
2022-09-16 15:37:40 +0200

    app marche bien

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
 
-const routes: Routes = [];
+import { PostsComponent } from './modules/posts/posts.component';
+
+const routes: Routes = [{
+  path: '', component: DefaultComponent,
+  children: [
+    { path: '', component: DashboardComponent },
+    { path: 'posts', component: PostsComponent }
+
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
